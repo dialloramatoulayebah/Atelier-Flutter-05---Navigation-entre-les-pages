c5a702ca97d38780cd07126f0142d94ca8782a8d
dialloramatoulayebah <129418622+dialloramatoulayebah@users.noreply.github.com>
2025-11-25 22:28:54 -0500

    Etape 3: Page d'info

---
+++ lib/page_infos.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class PageInfos extends StatelessWidget {
+  const PageInfos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Page d'infos", style: TextStyle(color: Colors.purple)),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Bienvenue sur la page d'infos!",
+                style: TextStyle(
+                  color: Colors.purple,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
