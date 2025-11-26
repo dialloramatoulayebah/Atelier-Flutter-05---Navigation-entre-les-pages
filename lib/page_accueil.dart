82d708c8ded727390d5ac85a281f0c32a2b9feb6
dialloramatoulayebah <129418622+dialloramatoulayebah@users.noreply.github.com>
2025-11-25 22:08:08 -0500

    Etape 2

---
+++ lib/page_accueil.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class PageAccueil extends StatelessWidget {
+  const PageAccueil({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Page d'accueil")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Bienvenue sur la page d\'accueil!",
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
